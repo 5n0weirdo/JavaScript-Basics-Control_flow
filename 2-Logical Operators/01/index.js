6ccf8673676d9a49ed141a7e657f1d68ae6ed832
5n0weirdo <snoweirdo@protonmail.com>
2021-09-19 09:50:34 +0200

    Logical Operators

---
+++ 2-Logical Operators/01/index.js
@@ -0,0 +1,11 @@
+// type the code in your console to evaluate the answer
+var x = 10;
+var y = "a";
+
+// y tripple equal to  a string 'b' OR x is bigger or equal to 10
+y === "b" || x >= 10 //true
+
+var x = 3;
+var y = 8;
+
+!(x == "3" ||  x == y) && !(y != 8 && x <= y) //false
---
+++ 2-Logical Operators/02/index.js
@@ -0,0 +1,16 @@
+//These are Falsy Values:
+false
+0
+""
+null
+undefined
+NaN
+
+//Everything else  is Truthy
+
+//Type this into your console to evaluate the answer
+var new = "";
+var msg = "haha!";
+var isWise = "false";
+
+!((str || msg) && isWise)
