0e827a5195d4da7bea0747c206b225851f143074
5n0weirdo <snoweirdo@protonmail.com>
2021-09-22 16:21:57 +0200

    Intro to Loops

---
+++ 5-Intro to Loops/01/index.js
@@ -0,0 +1,14 @@
+// While loops:
+// continues to run the code while the condition is true
+// while (someCondition) {
+  //run some code
+// }
+
+var count = 5;
+
+while (count <= 20) {
+  console.log("count is: " + count);
+
+  // count in 2's
+  count+=2;
+}
---
+++ 5-Intro to Loops/02/index.js
@@ -0,0 +1,20 @@
+// string we are looping:
+var name = "Developer";
+
+// first character is at index 0
+var count = 0;
+
+while (count < name.length) {
+  console.log(name[count]);
+  count++;
+}
+
+// D
+// e
+// v
+// e
+// l
+// o
+// p
+// e
+// r
