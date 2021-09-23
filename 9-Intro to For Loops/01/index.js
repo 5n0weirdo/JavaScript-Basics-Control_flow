d62cccb1303a484fd92957282896ce48a7c3fdc7
5n0weirdo <snoweirdo@protonmail.com>
2021-09-23 18:26:00 +0200

    Intro to For Loops

---
+++ 9-Intro to For Loops/01/index.js
@@ -0,0 +1,14 @@
+// for loop:
+// print numbers from 1 - 5
+for (var count = 1; count < 6; count++) {
+  console.log(count);
+}
+
+// while loop:
+// print numbers from 1- 5
+var betw = 1;
+
+while (betw < 6) {
+  console.log("Count is: " + betw);
+  betw++;
+}
---
+++ 9-Intro to For Loops/02/index.js
@@ -0,0 +1,15 @@
+// for loop:
+var greeting = "hey";
+
+for (var i = 0; i < greeting.length; i++) {
+  console.log(greeting[i]);
+}
+
+// while loop:
+var greets = "heya";
+var count = 0;
+
+while (count < greets.length) {
+  console.log(greets[count]);
+  count++;
+}
